91dbf995700b05b4e567a41a41bc66464d633e33
coolboy <3179215559@qq.com>
2024-11-14 01:32:40 +0800

    d

--- src/gd/cn/message/PicList.java
+++ src/gd/cn/message/PicList.java
@@ -3,17 +3,18 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
 @XmlRootElement(name="PicList")
+@XmlAccessorType(XmlAccessType.FIELD) // 直接绑定字段
 public class PicList {
-@XmlElement(name="PicMd5Sum")
-private String PicMd5Sum;
+	@XmlElement(name="PicMd5Sum")
+	private String PicMd5Sum;
 
-public String getPicMd5Sum() {
-	return PicMd5Sum;
-}
+	public String getPicMd5Sum() {
+		return PicMd5Sum;
+	}
 
-public void setPicMd5Sum(String picMd5Sum) {
-	PicMd5Sum = picMd5Sum;
-}
-
+	public void setPicMd5Sum(String picMd5Sum) {
+		PicMd5Sum = picMd5Sum;
+	}
 }
